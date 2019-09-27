1bae1a3cb3fcb7a284164d5daf75443edf331cb1
knokko <knokogator@hotmail.com>
2019-09-27 16:41:07 +0200

    Add TextColors struct

--- src/util/color.rs
+++ src/util/color.rs
@@ -8,7 +8,16 @@ pub struct Color {
 
 impl Color {
 
-    pub fn from_rgb(red: u8, green: u8, blue: u8) -> Color {
+    pub const BLACK: Color = Color::from_rgb(0, 0, 0);
+    pub const WHITE: Color = Color::from_rgb(255, 255, 255);
+
+    pub const RED: Color = Color::from_rgb(255, 0, 0);
+    pub const GREEN: Color = Color::from_rgb(0, 255, 0);
+    pub const BLUE: Color = Color::from_rgb(0, 0, 255);
+
+    pub const TRANSPARENT: Color = Color::from_rgba(0,0,0,0);
+
+    pub const fn from_rgb(red: u8, green: u8, blue: u8) -> Color {
         Color {
             red,
             green,
@@ -17,7 +26,7 @@ impl Color {
         }
     }
 
-    pub fn from_rgba(red: u8, green: u8, blue: u8, alpha: u8) -> Color {
+    pub const fn from_rgba(red: u8, green: u8, blue: u8, alpha: u8) -> Color {
         Color {
             red,
             green,
@@ -56,6 +65,47 @@ impl Color {
 
     pub fn get_alpha(&self) -> u8 {
         self.alpha
+    }
+}
+
+#[derive(Clone,Copy)]
+pub struct TextColors {
+
+    pub fill_color: Color,
+    pub stroke_color: Color,
+    pub background_color: Color
+}
+
+impl TextColors {
+
+    pub const fn new(fill_color: Color, stroke_color: Color, background_color: Color) -> TextColors {
+        TextColors {
+            fill_color,
+            stroke_color,
+            background_color
+        }
+    }
+
+    pub const fn create_label(fill_color: Color, stroke_color: Color) -> TextColors {
+        TextColors {
+            fill_color,
+            stroke_color,
+            background_color: Color::TRANSPARENT
+        }
+    }
+
+    pub const fn create_simple_label(color: Color) -> TextColors {
+        Self::create_label(color, color)
+    }
+
+    pub const BLACK_LABEL: TextColors = Self::create_simple_label(Color::BLACK);
+
+    pub const fn create_simple_button(background_color: Color) -> TextColors {
+        TextColors {
+            fill_color: Color::BLACK,
+            stroke_color: Color::BLACK,
+            background_color
+        }
     }
 }
 
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -2,7 +2,9 @@ use web_sys::console;
 
 use wasm_bindgen::JsValue;
 
-pub mod color;
+mod color;
+
+pub use color::*;
 
 pub fn print(message: &str){
     console::log_1(&JsValue::from_str(message));
